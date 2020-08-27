e026109210331874fa31972d81903a59cfc1ec41
Bader Fahoum <fahoumbader@gmail.com>
2020-08-27 12:13:38 +0300

    heroku fix

--- database/build.js
+++ database/build.js
@@ -5,9 +5,11 @@ const db = require("./connection");
 const initPath = path.join(__dirname, "init.sql");
 const initSQL = fs.readFileSync(initPath, "utf-8");
 
-db.query(initSQL)
-  .then(() => {
-    console.log("database created");
-    db.end();
-  })
-  .catch();
+exports.buildDB = () => {
+  db.query(initSQL)
+    .then(() => {
+      console.log("database created");
+      db.end();
+    })
+    .catch();
+};
